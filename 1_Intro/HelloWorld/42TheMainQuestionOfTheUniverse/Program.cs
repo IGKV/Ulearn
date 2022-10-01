c8e6566028b78792229b009a5500317d2addf87d
IGKV <kochetkov_i.n@outlook.com>
2022-10-02 00:51:10 +0700

    1 days/2

---
+++ 1_Intro/HelloWorld/42TheMainQuestionOfTheUniverse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TheUniverse
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Print(GetSquare(42));
+        }
+        public static int GetSquare(int a)
+        {
+            return a * a; // a = a*a
+}
+        static void Print(int b)
+        {
+            Console.WriteLine(b);
+        }
+    }
+
+}
+
