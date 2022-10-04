54e435dbe72898bc422e1e44cce5142a3fcb5726
IGKV <kochetkov_i.n@outlook.com>
2022-10-04 22:16:46 +0700

    3 eday

--- 1_Intro/HelloWorld/TasksForTheSeminar/Program.cs
+++ 1_Intro/HelloWorld/TasksForTheSeminar/Program.cs
@@ -1,7 +1,32 @@
-﻿namespace Tasks
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Tasks
 {
     class Program
     {
+
+        static void Main()
+        {
+            /* 
+            string a = "3"; //замена чисел местами
+            string b = "5";
+            Console.WriteLine(a + b);
+
+            (a, b) = (b, a);
+            Console.WriteLine(a + b);
+            ______________
+            int k = 231; //Число в обратном порядке 
+            Console.WriteLine(k.ToString().Reverse().ToArray());
+            __________________________
+            var chas = int.Parse(Console.ReadLine()); //Определение угла между часовой и минутной стрелкой.
+            var degres1 = chas * 30 - 360 * (chas / 12);
+            var degres = Math.Abs(360 * (degres1 / 180) - degres1);
+            Console.WriteLine(degres);
+            */
+
+
+        }
+
 
     }
 }
---
+++ 2_Error/Error/Types/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        var number = 5.5;
+        number += 7;
+        Console.WriteLine(number);
+    }
+
+}
